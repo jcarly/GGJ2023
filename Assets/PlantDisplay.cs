8a2eb7a568d136b83d28526110ca236492d3d14c
Jojo <jcarly23@gmail.com>
2023-02-05 14:25:45 +0100

    Display plant stad depending on score

---
+++ Assets/PlantDisplay.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantDisplay : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Script/Game/ScoreManager.cs
+++ Assets/Script/Game/ScoreManager.cs
@@ -8,6 +8,11 @@ public class ScoreManager : MonoBehaviour
     public Text scoreText;
     public static float score;
     public Player player;
+    public SpriteRenderer plantRenderer;
+    public List<Sprite> plantSprites;
+    public float speedIncrease = 1 / 150;
+    public float scoreThreshold = 200;
+
 
     // Update is called once per frame
     void Update()
@@ -15,11 +20,12 @@ public class ScoreManager : MonoBehaviour
         if(player && player.hp > 0)
         {
             score += 1 * Time.deltaTime;
-            Time.timeScale = 1 + 1 * (score / 150);
-            scoreText.text = ((int)score).ToString();
-            
+            Time.timeScale = 1 + 1 * score * speedIncrease;
+            scoreText.text = ((int)score).ToString();         
         } else {
             scoreText.text = "Score : " + ((int)score).ToString();
+            int spriteIndex = (int) (score / scoreThreshold);
+            if(plantRenderer) plantRenderer.sprite = plantSprites[spriteIndex > plantSprites.Count - 1 ? plantSprites.Count - 1 : spriteIndex];
         }
     }
 }
